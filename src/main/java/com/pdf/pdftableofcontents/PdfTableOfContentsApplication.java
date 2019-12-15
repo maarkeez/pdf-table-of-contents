66ee4b7c9e35225324e42acb72fb76c16c2358d4
maarkeez <d.marquez.delgado.91@gmail.com>
2019-12-15 09:56:34 +0100

    Initial commit

---
+++ src/main/java/com/pdf/pdftableofcontents/PdfTableOfContentsApplication.java
@@ -0,0 +1,13 @@
+package com.pdf.pdftableofcontents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfTableOfContentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfTableOfContentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pdf/pdftableofcontents/PdfTableOfContentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdf.pdftableofcontents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfTableOfContentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
